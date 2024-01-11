b04b5f1a220b254d422993f6a6295db04b396332
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-01-12 00:42:10 +0530

    Color Generator updated

--- 1RandomColorGenerator/ColorGenerator.js
+++ 1RandomColorGenerator/ColorGenerator.js
@@ -1,3 +1,14 @@
+// let element = document.getElementById("Color");
+// element.addEventListener("click", function () {
+//   var letters = '0123456789ABCDEF';
+//   var color = '#';
+//   for (var i = 0; i < 6; i++) {
+//     color += letters[Math.floor(Math.random() * 16)];
+//   }
+//   document.body.style.backgroundColor = color;
+//   element.innerHTML = color;
+// })
+
 function generateRandomColor() {
     const element = document.getElementById("Color")
     var letters = '0123456789ABCDEF';
