87f0e3428f8e8ccd75e7a8552dcece27f8684bb3
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-01-12 00:24:53 +0530

    Random Color Generator First Draft Completed

---
+++ 1RandomColorGenerator/ColorGenerator.js
@@ -0,0 +1,12 @@
+function generateRandomColor() {
+    const element = document.getElementById("Color")
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    document.body.style.backgroundColor = color;
+    element.innerHTML = color;
+}
+// Just a basic setup
+// keyboard spacebar change event is to be included
